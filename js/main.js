7df41f511bd98c0ee2b4560288b06e38035a99bf
Sam Brannon <sam@evolvelabs.com>
2015-05-10 12:17:03 -0500

    Added global background image.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 ---
 ---
+function equalHeight(group) {
+  var tallest = 0;
+  group.each(function() {
+    var thisHeight = $(this).height();
+    if(thisHeight > tallest) {
+      tallest = thisHeight;
+    }
+  });
+  group.height(tallest);
+}
+
 $(document).ready(function(){
   $("#header").addClass("scroll-header");
 });
